ed56b6c2950a891bb20a7b3d59dc1eaa7a272a34
Alperen Kantarcı <kantarcia@itu.edu.tr>
2025-10-19 22:57:05 +0200

    User login fully working. Header css has been fixed.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,8 +3,7 @@ import './Header.css';
 import logo from '../../assets/logo.png';
 import { Link } from 'react-router-dom';
 
-
-export default function Header({ onHowItWorksClick }) {
+export default function Header({ onHowItWorksClick, user, onLogout }) {
   return (
     <header className="haskify-header">
       <div className="header-content">
@@ -16,15 +15,17 @@ export default function Header({ onHowItWorksClick }) {
         </div>
       
         <nav className="header-nav">
+        <span className="user-id" style={{ alignSelf: 'center' }}>User: {user?.userId}</span>
           <a href="/" className="nav-link">Home</a>
           <button onClick={onHowItWorksClick} className="nav-link-button">How Does It Work?</button>
-        </nav>
+        
         <div className="user-info">
-        <span className="user-id">User: {user?.userId}</span>
-        <button onClick={onLogout} className="logout-button">
-          Logout
-        </button>
-      </div>
+          
+          <button onClick={onLogout} className="logout-button">
+            Logout
+          </button>
+        </div>
+        </nav>
       </div>
     </header>
   );
